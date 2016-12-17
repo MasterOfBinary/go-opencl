0f47d42135d4beeb27cfcae94d67fdf4e3c8e32e
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2016-12-17 14:59:51 +0800

    Remove include in opencl.go

--- opencl/opencl.go
+++ opencl/opencl.go
@@ -3,5 +3,4 @@ package opencl
 // #cgo !darwin CFLAGS: -I${SRCDIR}/external/include
 // #cgo !darwin LDFLAGS: -L${SRCDIR}/external/lib -lOpenCL
 // #cgo darwin LDFLAGS: -framework OpenCL
-// #include <CL/cl.h>
 import "C"
